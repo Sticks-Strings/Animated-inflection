b2bdf91615f301272780bde8d947dd0d9cb24f01
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-07-14 03:04:48 +0300

    make the foter

--- js/app.js
+++ js/app.js
@@ -1,91 +1,193 @@
+// 'use strict';
+// let arrProd = [];
+// let prodtot =0;
+// function Animated(image, pruductName, price) {
+//   this.image = image;
+//   this.pruductName = pruductName;
+//   this.price = price;
+//   arrProd.push(this);
+//   saveprodToLocalStorage();
+// }
+// let arrCart = [];
+// function CartAnimated(pruductName, price, quant) {
+//   this.pruductName = pruductName;
+//   this.price = price;
+//   this.quant = quant;
+//   arrCart.push(this);
+//   this.saveToLocalStorage();
+// }
+
+
+// let pickProd = function (event) {
+//   event.preventDefault();
+//   let whichProd = event.target.id;
+//   let s = whichProd.substring(1);
+//   let index = Number(s);
+//   let qv1 = event.target.q.value;
+//   console.log(arrProd[index].pruductName, arrProd[index].price, qv1);
+//   new CartAnimated(arrProd[index].pruductName, arrProd[index].price, qv1);
+//   console.log(arrCart);
+//   prodtot = prodtot+1 ;
+//   document.querySelector('.cart span').textContent=prodtot
+// };
+
+
+// function genarateobject() {
+//   new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
+//   new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
+//   new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
+//   new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
+//   new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120 JOD');
+//   new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
+//   new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
+//   new Animated('image/piano1.png', 'piano1', '20 JOD');
+//   new Animated('image/piano2.png', 'piano1', '30 JOD');
+//   new Animated('image/piano3.png', 'piano1', ' 30 JOD');
+//   new Animated('image/piano4.png', 'piano1', ' 30 JOD');
+//   new Animated('image/piano5.png', 'piano1', ' 30 JOD');
+//   new Animated('image/piano6.png', 'piano1', '30 JOD');
+//   new Animated('image/piano7.png', 'piano1', '30 JOD');
+//   new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
+//   new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
+//   new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
+//   new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
+//   new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120');
+//   new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
+//   new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
+//   new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
+//   new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
+//   new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
+//   new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
+//   new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120');
+//   new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
+//   new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
+//   new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
+//   new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
+//   new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
+//   new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
+//   new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120');
+//   new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
+//   new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
+//   new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
+// }
+// genarateobject();
+
+// function saveprodToLocalStorage() {
+//   let stringObj = JSON.stringify(arrProd);
+//   localStorage.setItem('prod', stringObj);
+// }
+
+
+// console.log(arrProd);
+
+// // add to local storage
+// CartAnimated.prototype.saveToLocalStorage = function () {
+//   let stringObj = JSON.stringify(arrCart);
+//   localStorage.setItem('cart', stringObj);
+  
+// }
+
+
 'use strict';
-let arrProd = [];
+let arrProd=[];
 let prodtot =0;
-function Animated(image, pruductName, price) {
-  this.image = image;
-  this.pruductName = pruductName;
-  this.price = price;
+function Animated (image, pruductName,price){
+  this.image=image;
+  this.pruductName=pruductName;
+  this.price=price;
   arrProd.push(this);
-  saveprodToLocalStorage();
+  saveprodToLocalStorage(this);
 }
-let arrCart = [];
-function CartAnimated(pruductName, price, quant) {
-  this.pruductName = pruductName;
-  this.price = price;
-  this.quant = quant;
+let arrCart=[];
+function CartAnimated ( pruductName,price,quant){
+  this.pruductName=pruductName;
+  this.price=price;
+  this.quant=quant;
   arrCart.push(this);
   this.saveToLocalStorage();
 }
-
-
-let pickProd = function (event) {
+let pickProd=function(event) {
   event.preventDefault();
   let whichProd = event.target.id;
   let s = whichProd.substring(1);
   let index = Number(s);
-  let qv1 = event.target.q.value;
-  console.log(arrProd[index].pruductName, arrProd[index].price, qv1);
-  new CartAnimated(arrProd[index].pruductName, arrProd[index].price, qv1);
+  let qv1=event.target.q.value;
+  console.log(arrProd[index].pruductName,arrProd[index].price, qv1);
+  new CartAnimated (arrProd[index].pruductName,arrProd[index].price, qv1);
   console.log(arrCart);
   prodtot = prodtot+1 ;
   document.querySelector('.cart span').textContent=prodtot
-  alert('you have successfully add  '+prodtot+ ' types of items ')
-
 };
-
-
 function genarateobject() {
-  new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
-  new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
-  new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
-  new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
-  new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120 JOD');
-  new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
-  new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
-  new Animated('image/piano1.png', 'piano1', '20 JOD');
-  new Animated('image/piano2.png', 'piano1', '30 JOD');
-  new Animated('image/piano3.png', 'piano1', ' 30 JOD');
-  new Animated('image/piano4.png', 'piano1', ' 30 JOD');
-  new Animated('image/piano5.png', 'piano1', ' 30 JOD');
-  new Animated('image/piano6.png', 'piano1', '30 JOD');
-  new Animated('image/piano7.png', 'piano1', '30 JOD');
-  new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
-  new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
-  new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
-  new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
-  new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120');
-  new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
-  new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
-  new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
-  new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
-  new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
-  new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
-  new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120');
-  new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
-  new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
-  new Animated('image/violin.1.png', 'D Z Strad Violin - Model 365', '200 JOD');
-  new Animated('image/violin.2.png', 'D Z Strad N201 Violin with', '250 JOD');
-  new Animated('image/violin.3.png', 'D Z Strad Violin - Model 402', '100 JOD');
-  new Animated('image/violin.4.png', 'Z Strad 4-string Electric 4/4', '320 JOD');
-  new Animated('image/violin.5.png', 'Scott Cao STV017 Violin', '120');
-  new Animated('image/violin.6.png', 'Scott Cao 950 Violin Cannon 1743', '500 JOD');
-  new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
-  new Animated('image/violin.7.png', 'Rene Jacquemin Violin, Mirecourt', '59 JOD');
+  new Animated('image/violin.1.png','D Z Strad Violin - Model 365' , 200);
+  new Animated('image/violin.2.png','D Z Strad N201 Violin with', 250);
+  new Animated('image/violin.3.png','D Z Strad Violin - Model 402', 100);
+  new Animated('image/violin.4.png','Z Strad 4-string Electric 4/4', 320);
+  new Animated('image/violin.5.png','Scott Cao STV017 Violin', '120');
+  new Animated('image/violin.6.png','Scott Cao 950 Violin Cannon 1743', 500);
+  new Animated('image/violin.7.png','Rene Jacquemin Violin, Mirecourt', 50);
+  new Animated('image/piano1.png','piano1', 30.000);
+  new Animated('image/piano2.png','piano1', 30.000);
+  new Animated('image/piano3.png','piano1',30.000);
+  new Animated('image/piano4.png','piano1',30.000);
+  new Animated('image/piano5.png','piano1',30.000);
+  new Animated('image/piano6.png', 'piano1',30.000);
+  new Animated('image/piano7.png','piano1', 30.000);
 }
 genarateobject();
-
 function saveprodToLocalStorage() {
   let stringObj = JSON.stringify(arrProd);
   localStorage.setItem('prod', stringObj);
 }
-
-
 console.log(arrProd);
-
 // add to local storage
+// CartAnimated.prototype.saveToLocalStorage = function (indexremove=[]) {
+//   let removeItemi = indexremove;
+//   let stringObj;
+//   let arrs =[];
+//   if (removeItemi !== undefined )
+//   {localStorage.removeItem('cart');
+//     for (let i =0; i<arrCart.length;i++)
+//     {
+//       if (removeItemi.indexOf(i) === -1)
+//       {
+//         arrs[i]=arrCart[i];
+//         stringObj = JSON.stringify(arrs);
+//       }
+//     }
+//   }
+//   else{
+//     stringObj = JSON.stringify(arrCart);
+//   }
+//   console.log( stringObj);
+//   localStorage.setItem('cart', stringObj);
+// };
+// CartAnimated.prototype.saveToLocalStorage = function (indexremove=[]) {
 CartAnimated.prototype.saveToLocalStorage = function () {
-  let stringObj = JSON.stringify(arrCart);
+  // let removeItemi = indexremove;
+  let stringObj;
+  // let arrs =[];
+  // if (removeItemi !== undefined )
+  // {localStorage.removeItem('cart');
+  //   for (let i =0; i<arrCart.length;i++)
+  //   {
+  //     if (removeItemi.indexOf(i) === -1)
+  //     {
+  //       arrs[i]=arrCart[i];
+  //       stringObj = JSON.stringify(arrs);
+  //     }
+  //   }
+  // }
+  // else{
+  stringObj = JSON.stringify(arrCart);
+  // }
+  console.log( stringObj);
   localStorage.setItem('cart', stringObj);
-  
-}
+};
+
+
+
+
+
+
 
--- js/cart.js
+++ js/cart.js
@@ -1,143 +1,119 @@
 'use strict';
 let table = document.getElementById('table');
 console.log(arrCart);
-
-// load from the local storage 
+let cartindex = [];
+let removeIndex = [];
+// load from the local storage
 function readlocalstorage() {
-    let stobj = localStorage.getItem('cart');
-    let normalobj = JSON.parse(stobj);
-
-    if (normalobj !== null) {
-
-        for (let x = 0; x < normalobj.length; x++) {
-            new CartAnimated(normalobj[x].pruductName, normalobj[x].price, normalobj[x].quant);
-            arrCart[x].rendertable();
-          
-        }
-    }}
-
-
-let th ;
-function creatTable() {
-    let head = document.createElement('tr');
-    th = document.createElement('th');
-    th.textContent = 'Product name';
-    head.appendChild(th);
-    th = document.createElement('th');
-    th.textContent = 'price';
-    head.appendChild(th);
-    th = document.createElement('th');
-    th.textContent = 'quant';
-    head.appendChild(th);
-    th = document.createElement('th');
-    th.textContent = 'Piece Total price';
-    head.appendChild(th);
-    th = document.createElement('th');
-    th.textContent = 'remove from cart';
-    head.appendChild(th);
-    table.appendChild(head);
-    // for (let i = 0; i < arrCart.length; i++) {
-    //     let tdEL = document.createElement('td');
-    //     th.textContent = arrCart[i].p;
-    //     head.appendChild(th);
-    // }
-    // let thel = document.createElement('th');
-    // thel.textContent = 'dailytotal';
-    // head.appendChild(thel);
-    // table.appendChild(head);
-    // let threm = document.createElement('th');
-    // threm.textContent = 'remove';
-    // head.appendChild(threm);
-    // table.appendChild(head);
+  arrCart = [];
+  let stobj = localStorage.getItem('cart');
+  let normalobj = JSON.parse(stobj);
+  if (normalobj !== null) {
+    for (let x = 0; x < normalobj.length; x++) {
+      new CartAnimated(normalobj[x].pruductName, normalobj[x].price, normalobj[x].quant);
+    }
+    rendertable();
+  }
 }
-
+let th;
+function creatTable() {
+  table.textContent = '';
+  let head = document.createElement('tr');
+  th = document.createElement('th');
+  th.textContent = 'Product name';
+  head.appendChild(th);
+  th = document.createElement('th');
+  th.textContent = 'price';
+  head.appendChild(th);
+  th = document.createElement('th');
+  th.textContent = 'quant';
+  head.appendChild(th);
+  th = document.createElement('th');
+  th.textContent = 'Piece Total price';
+  head.appendChild(th);
+  th = document.createElement('th');
+  th.textContent = 'remove from cart';
+  head.appendChild(th);
+  table.appendChild(head);
+}
 creatTable();
 let quanttotal;
-let Total=0;
-
+let Total;
 let tdEl = document.createElement('td');
 let removeButton = document.createElement('input');
 let removeEl = document.createElement('td');
-let key =[];
-let x=0;
-let cartindex;
+let key = [];
+let x = 0;
 let text = document.createElement('p');
-CartAnimated.prototype.rendertable = function () {
-    Total = 0;
-     let trEl;
-    // table.appendChild(trEl);
-    for (let y = 0; y < arrCart.length; y++) {
-        trEl = document.createElement('tr');
-        let td = document.createElement('td');
-        td.textContent = arrCart[y].pruductName ;
-        trEl.appendChild(td);
-       td = document.createElement('td');
-        td.textContent = arrCart[y].price ;
-        trEl.appendChild(td);
-        td = document.createElement('td');
-        if(arrCart[y].quant === "")
-        td.textContent = "1" ;
-        else
-        td.textContent = arrCart[y].quant ;
-        trEl.appendChild(td);
-        td = document.createElement('td');
-        if(arrCart[y].quant != ""){
-            quanttotal = arrCart[y].quant * arrCart[y].price;
-        td.textContent =  quanttotal;
-        Total +=  Number(quanttotal);
+let rendertable = function () {
+  Total = 0;
+  let trEl;
+  // table.appendChild(trEl);
+  for (let y = 0; y < arrCart.length; y++) {
+    trEl = document.createElement('tr');
+    trEl.setAttribute('id', `r${y}`);
+    let td = document.createElement('td');
+    td.textContent = arrCart[y].pruductName;
+    trEl.appendChild(td);
+    td = document.createElement('td');
+    td.textContent = arrCart[y].price;
+    trEl.appendChild(td);
+    td = document.createElement('td');
+    if (arrCart[y].quant === '')
+      td.textContent = '1';
+    else
+      td.textContent = arrCart[y].quant;
+    trEl.appendChild(td);
+    td = document.createElement('td');
+    if (arrCart[y].quant !== '') {
+      quanttotal = arrCart[y].quant * arrCart[y].price;
+      td.textContent = quanttotal;
+      Total += Number(quanttotal);
     }
-        else{
-          Total +=Number( arrCart[y].price);
+    else {
+        Total += Number(arrCart[y].price);
         td.textContent = arrCart[y].price;
-          
-        }
-        trEl.appendChild(td);
-        removeEl = document.createElement('td');
-   
-        removeButton = document.createElement('input');
-        removeButton.type = 'button';
-        removeButton.value = 'X';
-        removeButton.style.color = 'red';
-        removeButton.id = `c${y}`;
-        cartindex=removeButton.id;
-        removeButton.addEventListener('click',handl);
-        // key[y]= y;
-        // console.log(key[y]);
-        // removeButton.style.textAlign = 'center';
-        removeEl.appendChild(removeButton);
-        trEl.appendChild(removeEl);
-
-      
-
     }
+    trEl.appendChild(td);
+    removeEl = document.createElement('td');
+    removeButton = document.createElement('input');
+    removeButton.type = 'button';
+    removeButton.value = 'X';
+    removeButton.style.color = 'red';
+    removeButton.id = `c${y}`;
+    cartindex.push(removeButton.id);
+    removeButton.addEventListener('click', handl);
+    removeEl.appendChild(removeButton);
+    trEl.appendChild(removeEl);
     table.appendChild(trEl);
-
-    this.saveToLocalStorage();
-}
-
-console.log(removeButton);
-
-let indexc;
-function handl(event){
-    console.log('clicked');
-    // event.preventDefault();
-   let indext=cartindex.substring(1);
-   indexc=Number(indext);
-   console.log(`${indexc}`);
-    if(event.target.id === `c${indexc}`){
-     
-      removeButton.parentElement.parentElement.remove();
-    //  cart = {...cart,pruductName}
-    }
-    // removeButton.parentElement.parentElement.remove();
-    //  localStorage.removeItem();
-
+  }
+  renderTotal();
+  // CartAnimated.prototype.saveToLocalStorage(removeIndex);
+  CartAnimated.prototype.saveToLocalStorage();
+};
+function handl(event) {
+//   console.log('clicked');
+  event.preventDefault();
+  for (let i = 0; i < cartindex.length; i++) {
+    // let indext=cartindex[i];
+    let ev = event.target.id;
+    console.log(ev);
+    if (event.target.id === `c${i}`) {
+      Total =Total- Number(arrCart[i].price);
+      arrCart.splice(i,1);
+      let row = document.getElementById(`r${i}`);
+      row.parentNode.removeChild(row);
+        }
+  }
+  // CartAnimated.prototype.saveToLocalStorage(removeIndex);
+  CartAnimated.prototype.saveToLocalStorage();
+  renderTotal();
 }
 readlocalstorage();
-
-
-// removeButton.addEventListener('click',handl);
-text.textContent = `Your orders grand total is : ${Total}`;
-let TotalContainer = document.getElementById("grandTotal");
-TotalContainer.appendChild(text);
-console.log(Total);
+function renderTotal() {
+  text.textContent ='';
+  text.textContent = `Your orders grand total is : ${Total}`;
+  let TotalContainer = document.getElementById('grandTotal');
+  TotalContainer.textContent = '';
+  TotalContainer.appendChild(text);
+}
